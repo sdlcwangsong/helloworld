53276c5eb853bf82f6352ba95d6f4555fd3316b0
ws <ws@wsdeMacBook-Pro.local>
2017-09-23 09:06:22 +0800

    bbbb

--- baidu.py
+++ baidu.py
@@ -5,9 +5,9 @@ from selenium.webdriver.common.keys import Keys
 import urllib
 import time,json
 import os
-print("hello1")
-#l
 print("workd")
+print("helo")
+>>>>>>> bbbb
 browser = webdriver.Chrome()
 browser.get("https://www.baidu.com/")
 browser.find_element_by_css_selector("#u1 .lb").click()
